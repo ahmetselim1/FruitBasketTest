e7dfee6ddc631e9fd138a0c87c15283e205a7c71
ahmetselim1 <93852078+ahmetselim1@users.noreply.github.com>
2021-12-24 06:00:33 -0500

    last edit lgtm

--- src/test/java/pack/MyFruitBasket.java
+++ src/test/java/pack/MyFruitBasket.java
@@ -2,6 +2,7 @@ package pack;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -16,15 +17,15 @@ public class MyFruitBasket {
             //this line reads the csv file and puts it into a list, helped me easily to print
             List<String[]> fruitList = reader.readAll();
 
-
             int totalNumberOfFruit = 0;
             Set<String> uniqueListOfFruits = new TreeSet<>();
-            Set<String> over3Days = new TreeSet<>();
+            Set<String> characteristics = new TreeSet<>();
+            Map<String, Integer> over3map = new TreeMap<>();
+            Map<Integer, String> fruitSort = new TreeMap<>(Collections.reverseOrder());
+            Map<String, Integer> newMap = new TreeMap<>();
+
             String[] eachFruitArray;
             int eachTypeCount;
-
-            //for reversing to descending order, I used the reverseOrder()
-            Map<String, Integer> newMap = new TreeMap<>(Collections.reverseOrder());
 
             for (int i = 1; i < fruitList.size(); i++) {
 
@@ -34,11 +35,14 @@ public class MyFruitBasket {
                 //I added the fruits to set to make sure they are all unique
                 uniqueListOfFruits.add(fruit);
 
+                characteristics.add(eachFruitArray[1]+" "+eachFruitArray[0]+": "+eachFruitArray[2]+", "+eachFruitArray[3]);
                 eachTypeCount = Integer.parseInt(eachFruitArray[1]);
                 totalNumberOfFruit += eachTypeCount;
 
                 //if the fruit is over three days, I put it into the Set to prevent the duplicates
-                if (Integer.parseInt(eachFruitArray[4]) > 3) over3Days.add(fruit);
+                if (Integer.parseInt(eachFruitArray[4]) > 3) {
+                    over3map.put(fruit, eachTypeCount);
+                }
 
                 /*
                 I combined the fruit counts in the map. if the item is not listed in the map, I added.
@@ -48,40 +52,47 @@ public class MyFruitBasket {
                 else newMap.put(fruit, eachTypeCount + newMap.get(fruit));
             }
 
-            System.out.println("The total Number of Fruits = \t" + totalNumberOfFruit);
+            System.out.println("Total number of fruit = \t" + totalNumberOfFruit);
 
             int totalTypesOfFruit = uniqueListOfFruits.size();
-            System.out.println("The total Type of Fruits = \t" + totalTypesOfFruit + "\n");
+            System.out.println("Types of fruit = \t" + totalTypesOfFruit + "\n");
 
-            System.out.println("The Characteristic List of each fruit is as: ");
-            fruitList.forEach(x -> System.out.println(Arrays.toString(x)));
+            System.out.println("The number of each type of fruit in descending order = \t");
+            for (Map.Entry<String, Integer> entry : newMap.entrySet())
+                fruitSort.put(entry.getValue(), entry.getKey());
+            fruitSort.forEach((k, v) -> System.out.println(v + ": " + k));
 
-            System.out.println("Fruits that is over three days = \t" + over3Days);
-            System.out.println("Fruit list in descending order = \t" + newMap);
+            System.out.println("The characteristics (size, color, shape, etc.) of each fruit by type = ");
+            characteristics.forEach(x -> System.out.println(x));
+
+            System.out.print("Fruits that has been in the basket for over 3 days = ");
+            over3map.forEach((k, v) -> System.out.print(v + " " + k + ", "));
+            System.out.print("are over three days.\n\n");
         }
     }
 }
 
-//todo what I get in the console is a :
-    /*
-        The total Number of Fruits = 	69
-        The total Type of Fruits = 	6
-        The Characteristic List of each fruit is as:
-        [name, size, color, shape, days]
-        [orange, 4, red, round, 2]
-        [pineapple, 3, yellow, round, 5]
-        [grape, 10, white, flat, 8]
-        [pear, 5, white, round, 6]
-        [pear, 5, white, round, 6]
-        [apple, 20, red, flat, 1]
-        [orange, 2, orange , round, 3]
-        [grape, 10, green, round, 4]
-        [grapefruit, 3, green, flat, 1]
-        [apple, 5, red, round, 3]
-        [apple, 2, red, round, 2]
-        Fruits that is over three days = 	[grape, pear, pineapple]
-        Fruit list in descending order = 	{pineapple=3, pear=10, orange=6, grapefruit=3, grape=20, apple=27}
+//todo what it going to print is :
+/*
+Total number of fruit = 	69
+Types of fruit = 	6
 
-
-
-     */
+The number of each type of fruit in descending order =
+apple: 27
+grape: 20
+pear: 10
+orange: 6
+pineapple: 3
+The characteristics (size, color, shape, etc.) of each fruit by type =
+10 grape: green, round
+10 grape: white, flat
+2 apple: red, round
+2 orange: orange , round
+20 apple: red, flat
+3 grapefruit: green, flat
+3 pineapple: yellow, round
+4 orange: red, round
+5 apple: red, round
+5 pear: white, round
+Fruits that has been in the basket for over 3 days = 10 grape, 5 pear, 3 pineapple, are over three days.
+ */
